6dcd66ea02d914485a85d1b9813f11f0a0157633
hxllmvdx <jigoku.tyan@gmail.com>
2026-04-10 17:40:03 +0300

    feat: implemented config

--- services/backend/cmd/server/main.go
+++ services/backend/cmd/server/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"backend/internal/config"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	cfg := config.LoadConfig()
+
 	router := gin.Default()
 
 	router.POST("/generate")
+
+	router.Run(fmt.Sprintf(":%s", cfg.Port))
 }
